498652cbed003a502571bf386fc5ce3713925ca7
WAIGYIYANKYAW <waigyiyankyaw@gmail.com>
2020-10-04 12:21:03 +0630

    homepage

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router,Route, Switch } from 'react-router-dom';
+import { Home, Browse, Signin, Signup } from './pages';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import * as ROUTES from './constants/routes';
+
+export default function App() {
+    return (
+        <Router> 
+            <Route exact path={ROUTES.BROWSE}>
+                <Browse />
+            </Route>
+            <Route exact path={ROUTES.SIGN_IN}>
+                <Signin />
+            </Route>
+            <Route exact path={ROUTES.SIGN_UP}>
+                <Signup />
+            </Route>
+            <Route exact path={ROUTES.HOME}>
+                <Home />
+            </Route>
+        </Router>
+    );
 }
-
-export default App;
