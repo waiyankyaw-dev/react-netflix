12d8dae29a61abc7ecf106248b40a30be0f7e518
WAIGYIYANKYAW <waigyiyankyaw@gmail.com>
2020-10-06 09:50:21 +0630

    adding browse page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import { BrowserRouter as Router,Route, Switch } from 'react-router-dom';
-import { Home, Browse, Signin, Signup } from './pages';
-
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import { Home, Browse, SignIn, SignUp } from './pages';
 import * as ROUTES from './constants/routes';
+import { IsUserRedirect, ProtectedRoute } from './helpers/routes';
+import { useAuthListener } from './hooks';
 
-export default function App() {
-    return (
-        <Router> 
-            <Route exact path={ROUTES.BROWSE}>
-                <Browse />
-            </Route>
-            <Route exact path={ROUTES.SIGN_IN}>
-                <Signin />
-            </Route>
-            <Route exact path={ROUTES.SIGN_UP}>
-                <Signup />
-            </Route>
-            <Route exact path={ROUTES.HOME}>
-                <Home />
-            </Route>
-        </Router>
-    );
+export function App() {
+  const { user } = useAuthListener();
+
+  return (
+    <Router>
+      <Switch>
+        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_IN}>
+          <SignIn />
+        </IsUserRedirect>
+        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_UP}>
+          <SignUp />
+        </IsUserRedirect>
+        <ProtectedRoute user={user} path={ROUTES.BROWSE}>
+          <Browse />
+        </ProtectedRoute>
+        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.HOME}>
+          <Home />
+        </IsUserRedirect>
+      </Switch>
+    </Router>
+  );
 }
